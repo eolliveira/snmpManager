96815d35d0407a2e9004221994c557adcadf229d
Erick Oliveira <erick.oliveira9607@gmail.com>
2023-01-31 20:51:43 -0300

    particao disco

--- src/main/java/com/example/snmpManager/objects/HardDiskObject.java
+++ src/main/java/com/example/snmpManager/objects/HardDiskObject.java
@@ -2,12 +2,18 @@ package com.example.snmpManager.objects;
 
 import lombok.Data;
 
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
 @Data
-public class HardDiskObject {
+public class HardDiskObject implements Serializable {
     private String nome;
     private String modelo;
     private String numeroSerie;
     private String capacidade;
     private String usado;
     private String disponivel;
+
+    private List<PartitionObject> particoes = new ArrayList<>();
 }
---
+++ src/main/java/com/example/snmpManager/objects/PartitionObject.java
@@ -0,0 +1,11 @@
+package com.example.snmpManager.objects;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class PartitionObject implements Serializable {
+    private String pontoMontagem;
+    private String tamanho;
+}
