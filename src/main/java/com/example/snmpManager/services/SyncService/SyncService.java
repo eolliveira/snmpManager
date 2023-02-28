dfe22ba8f1fab329abdd8adb985689f7d85f21db
erickoliveira <erick.oliveira9607@gmail.com>
2023-02-28 15:44:56 -0300

    Refactor @RequiredArgsConstructor

--- src/main/java/com/example/snmpManager/services/SyncService/SyncService.java
+++ src/main/java/com/example/snmpManager/services/SyncService/SyncService.java
@@ -1,46 +1,20 @@
 package com.example.snmpManager.services.SyncService;
 
 import com.example.snmpManager.objects.TrapObject;
-import com.example.snmpManager.repositories.DiscoParticaoRepository;
-import com.example.snmpManager.repositories.DiscoRepository;
-import com.example.snmpManager.repositories.EstacaoTrabalhoRepository;
-import com.example.snmpManager.repositories.InterfaceRepository;
 import com.example.snmpManager.services.EstacaoTrabalhoService.SyncWorkstationByIpAddressService;
-import com.example.snmpManager.services.EstacaoTrabalhoService.GetDataFromWorkstationService;
-import com.example.snmpManager.services.EstacaoTrabalhoService.SyncWorkstationByAssetIdService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.Objects;
 
 @Service
+@RequiredArgsConstructor
 public class SyncService {
-
-    @Autowired
-    SyncWorkstationByIpAddressService syncWorkstationByIpAddressService;
-
-    @Autowired
-    InterfaceRepository interfaceRepository;
-
-    @Autowired
-    DiscoRepository discoRepository;
-
-    @Autowired
-    DiscoParticaoRepository discoParticaoRepository;
-
-    @Autowired
-    EstacaoTrabalhoRepository estacaoTrabalhoRepository;
-
-    @Autowired
-    GetDataFromWorkstationService getDataFromWorkstationService;
-
-    @Autowired
-    SyncWorkstationByAssetIdService syncWorkstationByAssetIdService;
-
+    private final SyncWorkstationByIpAddressService syncWorkstationByIpAddressService;
 
     public void checkAgentSync(TrapObject trapObject) {
         if (Objects.equals(trapObject.getTipoAtivo(), "WORKSTATION")) {
-           syncWorkstationByIpAddressService.synchronizeWorstation(trapObject.getIpAddress());
+            syncWorkstationByIpAddressService.synchronizeWorstation(trapObject.getIpAddress());
         }
     }
 
