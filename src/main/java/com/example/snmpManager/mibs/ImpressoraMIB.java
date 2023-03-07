66e0c437a6d5d0a88f5d59a0a6490796bbc04f23
erickoliveira <erick.oliveira9607@gmail.com>
2023-03-07 17:38:08 -0300

    Exception GetDataPrinter

--- src/main/java/com/example/snmpManager/mibs/ImpressoraMIB.java
+++ src/main/java/com/example/snmpManager/mibs/ImpressoraMIB.java
@@ -16,36 +16,12 @@ public class ImpressoraMIB {
     private final String DOMINIO_OID = ".1.3.6.1.4.1.11.2.4.3.5.46.0";
     private final String STATUS_OID = ".1.3.6.1.2.1.25.3.2.1.5.1";
 
-
-
-    /* STATUS DA IMPRESSORA(HP)
-        1: Other (Outros)
-        2: Unknown (Desconhecido)
-        3: Idle (Ociosa)
-        4: Printing (Imprimindo)
-        5: Warmup (Aquecendo)
-        6: Stopped Printing (Impressão interrompida)
-        7: Offline (Desligada)
-        8: Paused (Pausada)
-        9: Error (Erro)
-        10: Busy (Ocupada)
-        11: Not Available (Indisponível)
-        12: Waiting (Aguardando)
-        13: Processing Data (Processando dados)
-        14: Initialization (Inicializando)
-        15: Warming Up (Aquecendo)
-        16: Toner Low (Toner baixo)
-        17: No Toner (Sem toner)
-        18: Page Punt (Falta de página)
-        19: User Intervention Required (Intervenção do usuário necessária)
-        20: Out of Memory (Sem memória)
-        21: Door Open (Porta aberta)
-        22: Server Unknown (Servidor desconhecido)
-        23: Power Save (Economia de energia)
-        24: Server Restarting (Servidor reiniciando)
-        25: Server Required (Servidor necessário)
-        26: Server Unavailable (Servidor indisponível)
-    * */
-
+    /* status da impressora
+    *   unknown(1),
+        running(2),
+        warning(3),
+        testing(4),
+        down(5)
+    */
 
 }
--- src/main/java/com/example/snmpManager/services/ImpressoraService/GetDataFromPrinterService.java
+++ src/main/java/com/example/snmpManager/services/ImpressoraService/GetDataFromPrinterService.java
@@ -1,6 +1,8 @@
 package com.example.snmpManager.services.ImpressoraService;
 
+import com.example.snmpManager.exceptions.ClientConnectionFailedExecption;
 import com.example.snmpManager.exceptions.InvalidAddressExecption;
+import com.example.snmpManager.exceptions.UnableToGetDeviceDataException;
 import com.example.snmpManager.mibs.ImpressoraMIB;
 import com.example.snmpManager.objects.PrinterObject;
 import com.example.snmpManager.services.SNMPRequestClient;
@@ -19,34 +21,41 @@ public class GetDataFromPrinterService {
         if (!AddressValidation.isValidIpv4(address))
             throw new InvalidAddressExecption("Endereço ip [" + address + "] inválido!");
 
-        snmpRequestClient.start(address,"public");
 
         ImpressoraMIB mib = new ImpressoraMIB();
         PrinterObject printerObj = new PrinterObject();
 
-        String nome = snmpRequestClient.getAsString(new OID(mib.getNOME_OID()));
-        String modelo = snmpRequestClient.getAsString(new OID(mib.getMODELO_OID()));
-        String numeroSerie = snmpRequestClient.getAsString(new OID(mib.getNUMERO_SERIE_OID()));
-        String enderecoIp = snmpRequestClient.getAsString(new OID(mib.getENDERECO_IP() + address));
-        String mascaraSubRede = snmpRequestClient.getAsString(new OID(mib.getMASCARA_SUBREDE_OID() + address));
-        String enderecoMac = snmpRequestClient.getAsString(new OID(mib.getENDERECO_MAC_OID()));
-        String gateway = snmpRequestClient.getAsString(new OID(mib.getGATEWAY_OID()));
-        String tempoLigado = snmpRequestClient.getAsString(new OID(mib.getTEMPO_LIGADO_OID()));
-        String totalImpressoes = snmpRequestClient.getAsString(new OID(mib.getTOTAL_IMPRESSOES_OID()));
-        String status = snmpRequestClient.getAsString(new OID(mib.getSTATUS_OID()));
+        try {
+            snmpRequestClient.start(address, "public");
 
-        printerObj.setNome(nome);
-        printerObj.setModelo(modelo);
-        printerObj.setNumeroSerie(numeroSerie);
-        printerObj.setEnderecoIp(enderecoIp);
-        printerObj.setMascaraSubRede(mascaraSubRede);
-        printerObj.setEnderecoMac(enderecoMac);
-        printerObj.setGateway(gateway);
-        printerObj.setTempoLigado(tempoLigado);
-        printerObj.setTotalImpressoes(totalImpressoes);
-        printerObj.setStatus(status);
+            String nome = snmpRequestClient.getAsString(new OID(mib.getNOME_OID()));
+            String modelo = snmpRequestClient.getAsString(new OID(mib.getMODELO_OID()));
+            String numeroSerie = snmpRequestClient.getAsString(new OID(mib.getNUMERO_SERIE_OID()));
+            String enderecoIp = snmpRequestClient.getAsString(new OID(mib.getENDERECO_IP() + address));
+            String mascaraSubRede = snmpRequestClient.getAsString(new OID(mib.getMASCARA_SUBREDE_OID() + address));
+            String enderecoMac = snmpRequestClient.getAsString(new OID(mib.getENDERECO_MAC_OID()));
+            String gateway = snmpRequestClient.getAsString(new OID(mib.getGATEWAY_OID()));
+            String tempoLigado = snmpRequestClient.getAsString(new OID(mib.getTEMPO_LIGADO_OID()));
+            String totalImpressoes = snmpRequestClient.getAsString(new OID(mib.getTOTAL_IMPRESSOES_OID()));
+            String dominio = snmpRequestClient.getAsString(new OID(mib.getDOMINIO_OID()));
+            String status = snmpRequestClient.getAsString(new OID(mib.getSTATUS_OID()));
 
-        snmpRequestClient.close();
+            printerObj.setNome(nome);
+            printerObj.setModelo(modelo);
+            printerObj.setNumeroSerie(numeroSerie);
+            printerObj.setEnderecoIp(enderecoIp);
+            printerObj.setMascaraSubRede(mascaraSubRede);
+            printerObj.setEnderecoMac(enderecoMac);
+            printerObj.setGateway(gateway);
+            printerObj.setTempoLigado(tempoLigado);
+            printerObj.setTotalImpressoes(totalImpressoes);
+            printerObj.setStatus(status);
+            printerObj.setDominio(dominio);
+        } catch (ClientConnectionFailedExecption e) {
+            throw new UnableToGetDeviceDataException("Falha ao estabelecer conexão com o agente, pelo endereço ip [" + address + "]: " + e.getMessage());
+        } finally {
+            snmpRequestClient.close();
+        }
 
         return printerObj;
     }
