767bb02933c23425d4a0d1a1dcabcda5ac30a1dd
erickoliveira <erick.oliveira9607@gmail.com>
2023-03-10 15:33:09 -0300

    tratando converção json

--- src/main/java/com/example/snmpManager/objects/EstacaoTrabalhoObjects/WorkstationObject.java
+++ src/main/java/com/example/snmpManager/objects/EstacaoTrabalhoObjects/WorkstationObject.java
@@ -3,6 +3,7 @@ package com.example.snmpManager.objects.EstacaoTrabalhoObjects;
 import com.example.snmpManager.objects.DeviceObjectAbstract;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
+import com.google.gson.stream.MalformedJsonException;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
@@ -30,34 +31,65 @@ public class WorkstationObject extends DeviceObjectAbstract implements Serializa
     private List<ProgramObject> programas = new ArrayList<>();
 
     public void addPrinters(String arrayPrinters) {
-        Gson gson = new Gson();
-        List<PrinterObject> listImpressora = gson.fromJson(arrayPrinters, new TypeToken<List<PrinterObject>>(){}.getType());
-        listImpressora.stream().map(printer -> this.impressoras.add(printer)).collect(Collectors.toList());
+        try {
+            Gson gson = new Gson();
+            List<PrinterObject> listImpressora = gson.fromJson(arrayPrinters, new TypeToken<List<PrinterObject>>() {
+            }.getType());
+            listImpressora.stream().map(printer -> this.impressoras.add(printer)).collect(Collectors.toList());
+        } catch (Exception e) {
+            //TODO(Tratar)
+            System.out.println("Não foi possivel obter lista de impressoras do ativo");
+        }
+
     }
 
     public void addHardDisk(String arrayDisk) {
-        Gson gson = new Gson();
-        List<HardDiskObject> listDisk = gson.fromJson(arrayDisk, new TypeToken<List<HardDiskObject>>(){}.getType());
-        listDisk.stream().map(disk -> this.discos.add(disk)).collect(Collectors.toList());
+        try {
+            Gson gson = new Gson();
+            List<HardDiskObject> listDisk = gson.fromJson(arrayDisk, new TypeToken<List<HardDiskObject>>() {
+            }.getType());
+            listDisk.stream().map(disk -> this.discos.add(disk)).collect(Collectors.toList());
+        } catch (Exception e) {
+            //TODO(Tratar)
+            System.out.println("Não foi possivel obter lista de Discos Rigidos do ativo");
+        }
     }
 
     public void addInterfaces(String arrayInterfaces) {
-        Gson gson = new Gson();
+        try {
+            Gson gson = new Gson();
 
-        //TODO()Pode lançar exceção ao buscar enquanto agente esta inicializando
-        List<InterfaceObject> listInterfaces = gson.fromJson(arrayInterfaces, new TypeToken<List<InterfaceObject>>(){}.getType());
-        listInterfaces.stream().map(i -> super.getInterfaces().add(i)).collect(Collectors.toList());
+            //TODO()Pode lançar exceção ao buscar enquanto agente esta inicializando
+            List<InterfaceObject> listInterfaces = gson.fromJson(arrayInterfaces, new TypeToken<List<InterfaceObject>>() {
+            }.getType());
+            listInterfaces.stream().map(i -> super.getInterfaces().add(i)).collect(Collectors.toList());
+        } catch (Exception e) {
+            //TODO(Tratar)
+            System.out.println("Não foi possivel obter lista de interfaces do ativo");
+        }
     }
 
     public void addVideoCards(String arrayPlacasVideo) {
-        Gson gson = new Gson();
-        List<VideoCardObject> listVideoCards = gson.fromJson(arrayPlacasVideo, new TypeToken<List<VideoCardObject>>(){}.getType());
-        listVideoCards.stream().map(videoCard -> this.placasVideo.add(videoCard)).collect(Collectors.toList());
+        try {
+            Gson gson = new Gson();
+            List<VideoCardObject> listVideoCards = gson.fromJson(arrayPlacasVideo, new TypeToken<List<VideoCardObject>>() {
+            }.getType());
+            listVideoCards.stream().map(videoCard -> this.placasVideo.add(videoCard)).collect(Collectors.toList());
+        } catch (Exception e) {
+            //TODO(Tratar)
+            System.out.println("Não foi possivel obter lista de placas de video do ativo");
+        }
     }
 
     public void addSoftware(String arrayPrograms) {
-        Gson gson = new Gson();
-        List<ProgramObject> listPrograms = gson.fromJson(arrayPrograms, new TypeToken<List<ProgramObject>>(){}.getType());
-        listPrograms.stream().map(p -> this.programas.add(p)).collect(Collectors.toList());
+        try {
+            Gson gson = new Gson();
+            List<ProgramObject> listPrograms = gson.fromJson(arrayPrograms, new TypeToken<List<ProgramObject>>() {
+            }.getType());
+            listPrograms.stream().map(p -> this.programas.add(p)).collect(Collectors.toList());
+        } catch (Exception e) {
+            //TODO(Tratar)
+            System.out.println("Não foi possivel obter lista de programas do ativo");
+        }
     }
 }
